20ec57d89068fb46711f41dd1fece40e0a40922d
hung nguyen <hungnguyenhh291005@gmail.com>
2026-07-22 13:36:27 +0700

    hien thi san pham

---
+++ HuniCafe/Migrations/202607220300498_AddProductImage.cs
@@ -0,0 +1,18 @@
+﻿namespace HuniCafe.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProductImage : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "ImagePublicId", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "ImagePublicId");
+        }
+    }
+}
