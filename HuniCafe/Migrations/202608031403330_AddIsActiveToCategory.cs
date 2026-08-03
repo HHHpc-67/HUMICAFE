371ece4742e1387af20650c626a8dbece38a6a9c
Nguyễn Bình Minh <nbminh8950@gmail.com>
2026-08-03 23:17:00 +0700

    Big Change - +Category /Product

---
+++ HuniCafe/Migrations/202608031403330_AddIsActiveToCategory.cs
@@ -0,0 +1,20 @@
+﻿namespace HuniCafe.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsActiveToCategory : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Categories", "IsActive", c => c.Boolean(nullable: false));
+            AlterColumn("dbo.Categories", "CategoryDescription", c => c.String(maxLength: 500));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Categories", "CategoryDescription", c => c.String(maxLength: 100));
+            DropColumn("dbo.Categories", "IsActive");
+        }
+    }
+}
