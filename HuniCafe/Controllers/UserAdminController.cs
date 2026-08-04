0ba8b392b28678490c5ccbefa002719ee18f9fd5
hung nguyen <hungnguyenhh291005@gmail.com>
2026-08-04 21:14:01 +0700

    thêm form user

---
+++ HuniCafe/Controllers/UserAdminController.cs
@@ -0,0 +1,19 @@
+﻿using HuniCafe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HuniCafe.Controllers
+{
+    public class UserAdminController : Controller
+    {
+        private readonly HuniCafeDB db = new HuniCafeDB();
+        public ActionResult Index()
+        {
+            var users = db.Users.ToList();
+            return View(users);
+        }
+    }
+}
